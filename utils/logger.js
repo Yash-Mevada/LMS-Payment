c351dff24585d1e92d65a79d2a00af106e39b079
Yash-Mewada <yashmevada153@gmail.com>
2025-12-10 18:55:00 +0530

    Remove debug logging of request paths in logger middleware

--- utils/logger.js
+++ utils/logger.js
@@ -32,12 +32,6 @@ export const logger = (req, res, next) => {
   let responseData;
   const oldResponseData = res.json;
 
-  console.log(
-    "request path----------------",
-    req.path,
-    req.url,
-    req.originalUrl
-  );
   if (req.path === "/api/v1/audit-log") {
     return next();
   }
